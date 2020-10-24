01926669d1c3eb55115e5d4162c79c6d81b19258
dlebansais <dlebansais@gmail.com>
2020-10-24 19:42:28 +0200

    Add project files.

---
+++ ResourceTools/EmbeddedResourceLoader.cs
@@ -0,0 +1,18 @@
+﻿namespace ResourceTools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class EmbeddedResourceLoader<TResource>
+        where TResource : class
+    {
+        public static bool Load(out TResource value)
+        {
+            value = null!;
+            return false;
+        }
+    }
+}
---
+++ ResourceTools/ResourceTools.cs
@@ -0,0 +1,12 @@
+﻿namespace ResourceTools
+{
+    using System.Drawing;
+
+    public static class ResourceTools
+    {
+        public static bool LoadIcon(out Icon value)
+        {
+            return EmbeddedResourceLoader<Icon>.Load(out value);
+        }
+    }
+}
