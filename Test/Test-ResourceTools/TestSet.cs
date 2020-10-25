2f50344a4e64a84310d55c45915d233ae1e80d58
dlebansais <dlebansais@gmail.com>
2020-10-25 13:08:39 +0100

    Added coverage support.

---
+++ Test/Test-ResourceTools/TestSet.cs
@@ -0,0 +1,53 @@
+﻿namespace TestResourceTools
+{
+    using ResourceTools;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Reflection;
+    using System.Threading;
+    using System.IO;
+    using System.Drawing;
+
+    [TestFixture]
+    public class TestSet
+    {
+        [OneTimeSetUp]
+        public static void InitTestSession()
+        {
+            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = enUS;
+            CultureInfo.DefaultThreadCurrentUICulture = enUS;
+            Thread.CurrentThread.CurrentCulture = enUS;
+            Thread.CurrentThread.CurrentUICulture = enUS;
+
+            Assembly? ResourceToolsAssembly;
+
+            try
+            {
+                ResourceToolsAssembly = Assembly.Load("ResourceTools");
+            }
+            catch
+            {
+                ResourceToolsAssembly = null;
+            }
+            Assume.That(ResourceToolsAssembly != null);
+        }
+
+        #region Basic Tests
+        [Test]
+        public static void TestBasic()
+        {
+            bool Success;
+            Icon Icon;
+
+            Success = Loader.Load(string.Empty, string.Empty, out Icon);
+            Assert.That(!Success);
+
+            Success = Loader.LoadIcon(string.Empty, string.Empty, out Icon);
+            Assert.That(!Success);
+        }
+        #endregion
+    }
+}
---
+++ coverage.bat
@@ -0,0 +1,57 @@
+@echo off
+
+setlocal
+
+set PROJECTNAME=ResourceTools
+set TESTPROJECTNAME=Test-%PROJECTNAME%
+set RESULTFILENAME=Coverage-%PROJECTNAME%.xml
+set OPENCOVER_VERSION=4.7.922
+set OPENCOVER=OpenCover.%OPENCOVER_VERSION%
+set CODECOV_VERSION=1.12.2
+set CODECOV=Codecov.%CODECOV_VERSION%
+set NUINT_CONSOLE_VERSION=3.11.1
+set NUINT_CONSOLE=NUnit.ConsoleRunner.%NUINT_CONSOLE_VERSION%
+set FRAMEWORK=net48
+
+nuget install OpenCover -Version %OPENCOVER_VERSION% -OutputDirectory packages
+nuget install CodeCov -Version %CODECOV_VERSION% -OutputDirectory packages
+nuget install NUnit.ConsoleRunner -Version %NUINT_CONSOLE_VERSION% -OutputDirectory packages
+
+if not exist ".\packages\%OPENCOVER%\tools\OpenCover.Console.exe" goto error_console1
+if not exist ".\packages\%CODECOV%\tools\codecov.exe" goto error_console2
+if not exist ".\packages\%NUINT_CONSOLE%\tools\nunit3-console.exe" goto error_console3
+
+call ..\Certification\set_tokens.bat
+
+dotnet publish ./Test/%TESTPROJECTNAME% -c Debug -f %FRAMEWORK% /p:Platform=x64 -o ./Test/%TESTPROJECTNAME%/publish/x64/Debug
+dotnet publish ./Test/%TESTPROJECTNAME% -c Release -f %FRAMEWORK% /p:Platform=x64 -o ./Test/%TESTPROJECTNAME%/publish/x64/Release
+
+if not exist ".\Test\%TESTPROJECTNAME%\publish\x64\Debug\%TESTPROJECTNAME%.dll" goto error_not_built
+if not exist ".\Test\%TESTPROJECTNAME%\publish\x64\Release\%TESTPROJECTNAME%.dll" goto error_not_built
+if exist .\Test\%TESTPROJECTNAME%\*.log del .\Test\%TESTPROJECTNAME%\*.log
+if exist .\Test\%TESTPROJECTNAME%\obj\x64\Debug\%RESULTFILENAME% del .\Test\%TESTPROJECTNAME%\obj\x64\Debug\%RESULTFILENAME%
+if exist .\Test\%TESTPROJECTNAME%\obj\x64\Release\%RESULTFILENAME% del .\Test\%TESTPROJECTNAME%\obj\x64\Release\%RESULTFILENAME%
+".\packages\%OPENCOVER%\tools\OpenCover.Console.exe" -register:user -target:".\packages\%NUINT_CONSOLE%\tools\nunit3-console.exe" -targetargs:".\Test\%TESTPROJECTNAME%\publish\x64\Debug\%TESTPROJECTNAME%.dll --trace=Debug --labels=Before" -filter:"+[%PROJECTNAME%*]* -[%TESTPROJECTNAME%*]*" -output:".\Test\%TESTPROJECTNAME%\obj\x64\Debug\%RESULTFILENAME%"
+".\packages\%OPENCOVER%\tools\OpenCover.Console.exe" -register:user -target:".\packages\%NUINT_CONSOLE%\tools\nunit3-console.exe" -targetargs:".\Test\%TESTPROJECTNAME%\publish\x64\Release\%TESTPROJECTNAME%.dll --trace=Debug --labels=Before" -filter:"+[%PROJECTNAME%*]* -[%TESTPROJECTNAME%*]*" -output:".\Test\%TESTPROJECTNAME%\obj\x64\Release\%RESULTFILENAME%"
+if exist .\Test\%TESTPROJECTNAME%\obj\x64\Debug\%RESULTFILENAME% .\packages\%CODECOV%\tools\codecov -f ".\Test\%TESTPROJECTNAME%\obj\x64\Debug\%RESULTFILENAME%" -t %RESOURCETOOLS_CODECOV_TOKEN%
+if exist .\Test\%TESTPROJECTNAME%\obj\x64\Release\%RESULTFILENAME% .\packages\%CODECOV%\tools\codecov -f ".\Test\%TESTPROJECTNAME%\obj\x64\Release\%RESULTFILENAME%" -t %RESOURCETOOLS_CODECOV_TOKEN%
+goto end
+
+:error_console1
+echo ERROR: OpenCover.Console not found.
+goto end
+
+:error_console2
+echo ERROR: Codecov not found.
+goto end
+
+:error_console3
+echo ERROR: nunit3-console not found.
+goto end
+
+:error_not_built
+echo ERROR: %TESTPROJECTNAME%.dll not built (both Debug and Release are required).
+goto end
+
+:end
+del *.log
